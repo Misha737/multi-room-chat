266da0bb1bb9478b26227cf0f652022bf95ceea2
Misha <makovkamisha10@gmail.com>
2026-04-09 21:20:09 +0300

    Added tests for ClintInfo

---
+++ Server.Tests/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using Xunit;
+using Server;
+using Server.Messages;
+using System.Text;
+
+namespace Server.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void ClientInfoDeserialisation_RightRawBytes_ClientInfoIsCorrect()
+    {
+        int Id = 555;
+        string Name = "TestName";
+        byte[] Bytes = [.. BitConverter.GetBytes(Id), .. Encoding.UTF8.GetBytes(Name)];
+
+        ClientInfo clientInfo = ClientInfo.Deserialize(Bytes);
+        Assert.Equal(Id, clientInfo.Id);
+        Assert.Equal(Name, clientInfo.Name);
+    }
+
+    [Fact]
+    public void ClientInfoSerialisation_ClientInfo_RawBytesIsEqual()
+    {
+        int Id = 555;
+        string Name = "TestName";
+        byte[] Bytes = [.. BitConverter.GetBytes(Id), .. Encoding.UTF8.GetBytes(Name)];
+
+        ClientInfo clientInfo = new ClientInfo(Id, Name);
+        byte[] actual = clientInfo.Serialize();
+
+        Assert.Equal(Bytes, actual);
+    }
+
+    [Fact]
+    public void JoinRoomDeserialisation_RightRawBytes_JoinRoomIsCorrect()
+    {
+        byte tag = (byte)Tag.JoinRoom;
+        int Id = 555;
+        string Name = "TestName";
+        byte[] Bytes = [tag, .. BitConverter.GetBytes(4 + Name.Length), .. BitConverter.GetBytes(Id), .. Encoding.UTF8.GetBytes(Name)];
+
+        JoinRoom joinRoom = JoinRoom.Deserialize(Bytes);
+    }
+}
